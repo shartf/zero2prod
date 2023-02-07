dffabd7891a640e10aaefb80e2a09d0b8ab3b867
shartf <shartf@users.noreply.github.com>
2023-02-07 22:20:40 +0100

    Forgot to add files

---
+++ tests/health_check.rs
@@ -0,0 +1,31 @@
+//! tests/health_check.rs
+
+// `tokio::test` is the testing equivalent of `tokio::main`.
+// It also spares you from having to specify the `#[test]` attribute.
+//
+// You can inspect what code gets generated using
+// `cargo expand --test health_check` (<- name of the test file)”
+
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    spawn_app().await.expect("Failed to spawn the app.");
+    // reqwest to perform HTTP requests
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+// Launch our application in the background
+async fn spawn_app() -> std::io::Result<()> {
+    todo!()
+}
